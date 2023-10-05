374aa525d8cce4e3b04e0a7a1eb5f209682c1fad
adenletchworth <adenrletchworth@gmail.com>
2023-10-05 09:00:05 -0700

    feat(directory) initialized spring directory

---
+++ Isacariot-Member-System/src/main/java/com/isacariotsystems/IsacariotMemberSystem/IsacariotMemberSystemApplication.java
@@ -0,0 +1,13 @@
+package com.isacariotsystems.IsacariotMemberSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsacariotMemberSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsacariotMemberSystemApplication.class, args);
+	}
+
+}
---
+++ Isacariot-Member-System/src/test/java/com/isacariotsystems/IsacariotMemberSystem/IsacariotMemberSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isacariotsystems.IsacariotMemberSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsacariotMemberSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
