31605f7bb81fad703e5ea10a4922193768042c82
adenletchworth <adenrletchworth@gmail.com>
2023-10-10 11:15:22 -0700

    chore: changed import due to file name change

--- MemberSystem/src/main/java/com/isacariotsystems/MemberSystem/repository/MemberRepository.java
+++ MemberSystem/src/main/java/com/isacariotsystems/MemberSystem/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.isacariotsystems.MemberSystem.repository;
 
-import com.isacariotsystems.MemberSystem.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.isacariotsystems.MemberSystem.entity.Member;
 
 
 @Repository
