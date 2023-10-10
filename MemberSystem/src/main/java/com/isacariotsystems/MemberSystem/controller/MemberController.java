2dbc5d17f9f61767e8b0565e5b2359bb9385d682
adenletchworth <adenrletchworth@gmail.com>
2023-10-10 11:12:02 -0700

    feat: added get all members method

--- MemberSystem/src/main/java/com/isacariotsystems/MemberSystem/controller/MemberController.java
+++ MemberSystem/src/main/java/com/isacariotsystems/MemberSystem/controller/MemberController.java
@@ -1,12 +1,17 @@
 package com.isacariotsystems.MemberSystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.isacariotsystems.MemberSystem.model.Member;
+import com.isacariotsystems.MemberSystem.entity.Member;
 import com.isacariotsystems.MemberSystem.service.MemberService;
 
 @RestController
@@ -21,6 +26,11 @@ public class MemberController {
         memberService.saveMember(member);
 
         return "Sucessfully Added Member";
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<Member>> getAllMembers(){
+        return new ResponseEntity<List<Member>>(memberService.allMembers(),HttpStatus.OK);
     }
 
 }
