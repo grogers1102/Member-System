08e96a06f3d2bdeb42424bb1128e35013b1da3c7
adenletchworth <adenrletchworth@gmail.com>
2023-11-07 16:34:28 -0800

    feat: added logout functionality

--- MemberSystem/src/main/resources/static/js/portal.js
+++ MemberSystem/src/main/resources/static/js/portal.js
@@ -10,3 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
         // Something else
     }
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+    const logoutButton = document.querySelector(".log-out a");
+    logoutButton.addEventListener("click", (event) => {
+        const token = localStorage.getItem('token');
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (token || refreshToken){
+            localStorage.removeItem('token')
+            localStorage.removeItem('refreshToken')
+        }
+        window.location.href = '../index.html';
+    });
+})
+
+
+    
+    function submitLogoutForm(){
+        const token = localStorage.getItem('token');
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        if (token || refreshToken){
+            localStorage.removeItem('token');
+            localStorage.removeItem('refreshToken');
+        }
+
+        window.location.href='index.html'
+    }
