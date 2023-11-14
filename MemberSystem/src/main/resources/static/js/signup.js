73dd29428ef7b39ba61660965ad9a0a2fba678df
adenletchworth <adenrletchworth@gmail.com>
2023-11-13 18:12:14 -0800

    fix(js): minor error in signup params

--- MemberSystem/src/main/resources/static/js/signup.js
+++ MemberSystem/src/main/resources/static/js/signup.js
@@ -9,9 +9,15 @@ function addSignupEvenListener() {
     });
 }
 
-function signup() {
+function getInputValue(inputName) {
+    return document.getElementById(inputName).value;
+}
 
-    const urlNeeded = 'api/v1/auth/signup';
+function isFieldEmpty(value) {
+    return value.trim() === '';
+}
+
+function signup() {
 
     const [firstName, lastName, email, phoneNumber, address, password, confirmPassword] = [
         'firstName',
@@ -23,48 +29,61 @@ function signup() {
         'confirmPassword'
     ].map(getInputValue);
 
-    const fields = [firstName, lastName, email, phoneNumber, address, password, confirmPassword];
-
-    if (fields.some(isFieldEmpty)) {
+    if (firstName === '' || lastName === '' || email === '' || phoneNumber === '' || address === '' || password === '' || confirmPassword === '') {
         alert("Please enter all fields");
     } else if (password !== confirmPassword) {
         alert("Passwords don't match");
-    }
-
-    const params = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        phoneNumber: phoneNumber.value,
-        address: address.value,
-        password: password.value
-    };
-
-    fetch(urlNeeded, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params),
-    })
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('There was a problem with the request.');
-        }
-    })
-    .then(userObj => {
-
-        const { token, refreshToken, userId } = userObj;
+    } else {
         
-        localStorage.setItem('token', token);
-        localStorage.setItem('refreshToken', refreshToken);
-        localStorage.setItem('userId', userId);
-
-        window.location.href = '/portalPages/portal.html';
-    })
-    .catch(error => {
-        alert(error.message);
-    });
+        const userParams = {
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            address,
+            password
+        };
+        
+        makeSignupRequest(userParams);
+    }
 }
+    
+async function makeSignupRequest(userParams) {
+    const urlNeeded = 'api/v1/auth/signup';
+
+    try {
+        const response = await fetch(urlNeeded, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userParams),
+        });
+
+        if (response.ok) {
+            const userObj = await response.json();
+
+            const { token, refreshToken, userId } = userObj;
+
+            localStorage.setItem('token', token);
+            localStorage.setItem('refreshToken', refreshToken);
+            localStorage.setItem('userId', userId);
+
+            window.location.href = '/portalPages/portal.html';
+        } else {
+            if (response.status === 400) {
+                throw new Error('Bad request: Please check your inputs.');
+            } else if (response.status === 401) {
+                throw new Error('Unauthorized: Invalid credentials.');
+                window.localStorage.href='../error/401.html';
+            } else {
+                throw new Error('There was a problem with the request.');
+            }
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+
+
